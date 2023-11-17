71b205cdc84a00457b06d678dea8ebf7a0dd7650
Ahmad Afsharinejad <a.Afsharinejad@mohaymen.ir>
2023-11-17 21:29:39 +0330

    test

--- src/Services/WorkFlow/Workflow.Api/Program.cs
+++ src/Services/WorkFlow/Workflow.Api/Program.cs
@@ -1,10 +1,13 @@
+using Newtonsoft.Json;
 using Workflow.Application.Di;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+builder.Services.AddControllers()
+    .AddNewtonsoftJson(options => { options.SerializerSettings.TypeNameHandling = TypeNameHandling.All; });
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
